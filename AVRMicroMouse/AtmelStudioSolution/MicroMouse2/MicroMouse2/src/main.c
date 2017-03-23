70376fe14702c0b2a155a8bf31fab4a46b8c1c53
Robert Curran <ro5635@gmail.com>
2017-03-23 19:35:06 +0000

    Solved Timer Issues and Clock Scale Issue Discovered

--- AVRMicroMouse/AtmelStudioSolution/MicroMouse2/MicroMouse2/src/main.c
+++ AVRMicroMouse/AtmelStudioSolution/MicroMouse2/MicroMouse2/src/main.c
@@ -23,18 +23,19 @@ void startUp(void);
 void startUp(void){
 	
 	board_init();
-	sei();
+//	
 	InitMotorRegisters();
 	
 	//Set up the arrays of reflectance sensor
 	initReflectanceArray();
 	
 	//Calibrate the opto sensor
-	GLOBAL_OptoSetPoint =  33;//getCalibratedSetPoint();
+	GLOBAL_OptoSetPoint =  55;//getCalibratedSetPoint();
 	
 	_delay_ms(2500);
-	signalMotor();
-		
+	sei();
+	
+	signalMotor();	
 	
 }
 
@@ -43,32 +44,25 @@ int main(void){
 	//Set Up The Robot 
 	startUp();
 	
-	while (1)
-	{
-		startUp();
-	}
-	
-	
 	int rate = 0;
 	while(1){
 			
-	rate = rate + 10;
-	//Spin motor to denote calibration complete
-	LH_Motor_Duty = (rate);
-	RH_Motor_Duty = (rate*0.89);
-	Motor_Duty_ptr = &LH_Motor_Duty;			// Motor_Duty_ptr points to LH_Motor_Duty
-	OCR1A = *Motor_Duty_ptr;					// AIN2 = LH_Motor_Duty
-	Motor_Duty_ptr = &RH_Motor_Duty;
-	OCR2A = *Motor_Duty_ptr;					// BIN1
-
-			
-	if(rate > 250){
-		rate = 10;
-	}
 	
-	_delay_ms(10);
 	
 	}
 
 }
 
+
+
+
+
+/************************************************************************/
+/*								ISR Routines                            */
+/************************************************************************/
+
+ISR(TIMER0_OVF_vect)
+{
+	//Increment the value of optocount reached, used to make a kind of ad hock timer...
+	int_Opto_Count_Reached++;
+}
